f185481acc9fc85b15438b6d56b5f31d398ed2f0
emilieweiss <emilieweiss@outlook.com>
2025-11-11 17:47:09 +0100

    billedefix

--- src/components/ui/ImageCarousel.tsx
+++ src/components/ui/ImageCarousel.tsx
@@ -4,13 +4,13 @@ export default function ImageCarousel() {
   return (
     <Carousel className="w-screen" opts={{ loop: true }}>
       <CarouselContent>
-        <CarouselItem><img src="/src/assets/homepage/plus_carousel1.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
-        <CarouselItem><img src="/src/assets/homepage/plus_carousel2.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
-        <CarouselItem><img src="/src/assets/homepage/plus_carousel3.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
-        <CarouselItem><img src="/src/assets/homepage/plus_carousel4.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
-        <CarouselItem><img src="/src/assets/homepage/plus_carousel6.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
-        <CarouselItem><img src="/src/assets/homepage/plus_carousel7.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
-        <CarouselItem><img src="/src/assets/homepage/plus_carousel8.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
+        <CarouselItem><img src="/homepage/plus_carousel1.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
+        <CarouselItem><img src="/homepage/plus_carousel2.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
+        <CarouselItem><img src="/homepage/plus_carousel3.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
+        <CarouselItem><img src="/homepage/plus_carousel4.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
+        <CarouselItem><img src="/homepage/plus_carousel6.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
+        <CarouselItem><img src="/homepage/plus_carousel7.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
+        <CarouselItem><img src="/homepage/plus_carousel8.jpg" className="rounded-lg w-full h-[calc(100vh-120px)] object-cover" /></CarouselItem>
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
