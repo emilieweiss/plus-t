42238d01a970ab04f16c5ef9d1c8876bf8be1200
emilieweiss <emilieweiss@outlook.com>
2025-11-13 16:36:16 +0100

    First test ready

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/contact")({
   component: () => (
     <>
-      <div className="mb-6 flex flex-col items-center gap-6 px-4 pt-2 md:pt-8">
+      <div className="mb-3 flex flex-col items-center gap-2 px-4 pt-2 md:mb-6 md:gap-6 md:pt-8">
         <h1 className="text-center">Kontakt PLUS T</h1>
         <p className="max-w-3xl text-center">
           Har du nogle spørgsmål til PLUS T? Vil du høre mere om tidligere
@@ -13,41 +13,53 @@ export const Route = createFileRoute("/contact")({
       </div>
 
       <div className="flex items-center justify-center">
-        <div className="flex w-full max-w-[1520px] flex-col items-center justify-center gap-20 md:flex-row">
+        <div className="flex w-full max-w-[1520px] flex-col items-center justify-center gap-8 md:flex-row md:gap-20">
           {/* Aksel kolonne */}
-          <div className="flex max-w-md flex-col items-center">
+          <div className="flex max-w-md flex-col items-center px-6 md:px-0">
             <img
               src="/homepage/aksel.jpg"
               alt="Aksel"
-              className="mb-6 h-[400px] w-[300px] rounded-lg object-cover"
+              className="mb-3 h-[250px] w-[175px] rounded-lg object-cover md:mb-6 md:h-[400px] md:w-[300px]"
             />
-            <h2 className="mt-6 text-center">Aksel Buur Christensen</h2>
+            <h2 className="mt-3 text-center md:mt-6">Aksel Buur Christensen</h2>
             <p className="text-center">
               Aksel har været en del af PLUS T siden 2019, hvor han har været
               vejleder og kursusleder. Når Aksel ikke laver PLUS T, læser han
               til civilingeniør i København.
             </p>
-            <p className="mt-4 font-semibold">Kontakt</p>
-            <p>Mail: aksel.buur@hotmail.com</p>
-            <p>Tlf.: 2923 4567</p>
+            <ul className="mt-2 list-none text-center leading-snug md:mt-4">
+              <li className="font-semibold">Kontakt</li>
+              <li className="mb-4; text-sm font-light md:text-base">
+                Mail: aksel.buur@hotmail.com
+              </li>
+              <li className="mb-4; text-sm font-light md:text-base">
+                Tlf.: 2923 4567
+              </li>
+            </ul>
           </div>
 
           {/* Thea kolonne */}
-          <div className="flex max-w-md flex-col items-center">
+          <div className="flex max-w-md flex-col items-center px-6 md:px-0">
             <img
               src="/homepage/thea.jpg"
               alt="Thea"
-              className="mb-6 h-[400px] w-[300px] rounded-lg object-cover"
+              className="mb-3 h-[250px] w-[175px] rounded-lg object-cover md:mb-6 md:h-[400px] md:w-[300px]"
             />
-            <h2 className="mt-6 text-center">Thea Swane Lund </h2>
+            <h2 className="mt-3 text-center md:mt-6">Thea Swane Lund </h2>
             <p className="text-center">
               Thea har været en del af PLUS T siden 2024, hvor hun har været
               førstehjælpsansvarlig og vejleder. Når Thea ikke laver PLUS T,
               læser hun til sygeplejerske i København.
             </p>
-            <p className="mt-4 font-semibold">Kontakt</p>
-            <p>Mail: thea.swane@gmail.com </p>
-            <p>Tlf.: 21152974 </p>
+            <ul className="mt-2 list-none text-center leading-snug md:mt-4">
+              <li className="font-semibold">Kontakt</li>
+              <li className="mb-4; text-sm font-light md:text-base">
+                Mail: thea.swane@gmail.com
+              </li>
+              <li className="mb-4; text-sm font-light md:text-base">
+                Tlf.: 21152974{" "}
+              </li>
+            </ul>
           </div>
         </div>
       </div>
--- src/routes/sign_up/sign_up.tsx
+++ src/routes/sign_up/sign_up.tsx
@@ -1,26 +1,42 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/sign_up/sign_up')({
+export const Route = createFileRoute("/sign_up/sign_up")({
   component: () => (
-    <div className="flex justify-center items-center min-h-[calc(100vh-130px)]">
-      <div className="flex flex-col md:flex-row gap-8 max-w-[1520px] w-full items-center">
+    <div className="flex min-h-[calc(100vh-130px)] items-center justify-center">
+      <div className="flex w-full max-w-[1520px] flex-col items-center gap-8 md:flex-row">
         {/* Venstre kolonne - Billede (øverst på mobil) */}
         <div className="md:w-3/5">
-          <img 
-            src="/homepage/signup.jpg" 
-            alt="PLUS T tilmelding" 
-            className="w-full h-auto object-cover rounded-lg"
+          <img
+            src="/homepage/signup.jpg"
+            alt="PLUS T tilmelding"
+            className="h-auto w-full rounded-lg object-cover"
           />
         </div>
-        
+
         {/* Højre kolonne - Tekst (nedenunder på mobil) */}
-        <div className="md:w-2/5 text-white pr-4">
-          <h2 className="text-2xl md:text-[40px]">Sådan tilmelder du dig PLUS T</h2>
-          <p>Tilmeldingen til PLUS T åbner midt/slut maj, så sæt kryds i kalenderen allerede nu, hvis du vil sikre dig en plads på årets fedeste kursus.</p> 
-          <p>Når tilmeldingen åbner, foregår det gennem Det Danske Spejderkorps' hjemmeside. Du finder alt det praktiske og selve tilmeldingen her: https://dds.dk/arrangement/plus-start-plus-t-1 </p>
-          <p>Indtil da kan du følge med på vores sociale medier, hvor vi giver besked, så snart tilmeldingen åbner. Husk også at tale med din leder eller gruppes bestyrelse om tilskud. Mange grupper og divisioner støtter nemlig spejdere, der vil af sted på ungdomskursus. Check vores udførlige guide ud du finder her på hjemmesiden til hvordan.</p>
+        <div className="px-6 text-white md:w-2/5 md:pr-12 md:pl-0">
+          <h2 className="text-[22px] md:text-[40px]">
+            Sådan tilmelder du dig PLUS T
+          </h2>
+          <p>
+            Tilmeldingen til PLUS T åbner midt/slut maj, så sæt kryds i
+            kalenderen allerede nu, hvis du vil sikre dig en plads på årets
+            fedeste kursus.
+          </p>
+          <p>
+            Når tilmeldingen åbner, foregår det gennem Det Danske Spejderkorps'
+            hjemmeside. Du finder alt det praktiske og selve tilmeldingen her:
+            https://dds.dk/arrangement/plus-start-plus-t-1{" "}
+          </p>
+          <p>
+            Indtil da kan du følge med på vores sociale medier, hvor vi giver
+            besked, så snart tilmeldingen åbner. Husk også at tale med din leder
+            eller gruppes bestyrelse om tilskud. Mange grupper og divisioner
+            støtter nemlig spejdere, der vil af sted på ungdomskursus. Check
+            vores udførlige guide ud du finder her på hjemmesiden til hvordan.
+          </p>
         </div>
       </div>
     </div>
-  )
-})
+  ),
+});
